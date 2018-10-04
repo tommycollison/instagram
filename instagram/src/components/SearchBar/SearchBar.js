3a449aadacb7e4747a85dfc05e3b24b1a95eb951
Tommy Collison <tommy@collison.ie>
2018-10-03 20:25:52 -0700

    last commit before standup

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './SearchBar.css'
 import logo from './../../assets/ig_search_bar.png';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SearchBar = () => {
     return (
@@ -11,6 +12,11 @@ const SearchBar = () => {
                 <form>
                  <input type='search' placeholder='Search'></input>
                 </form>
+                <div className="icons">
+                {/* <FontAwesomeIcon icon="compass" />
+                <FontAwesomeIcon icon="heart" />
+                <FontAwesomeIcon icon="user" /> */}
+                </div>
         </div>
         </div>
     )
