248f64f9195dc0af80c89e0f5186c73ff1206b78
Tommy Collison <tommy@collison.ie>
2018-10-06 09:56:34 -0700

    search bar MVP done

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import './SearchBar.css'
+import './SearchBar.css';
 import logo from './../../assets/ig_search_bar.png';
+import heart from './../../assets/like.png';
+import user from './../../assets/Sed-01-512.png';
+import compass from './../../assets/compass-icon-22.png';
+
+
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SearchBar = () => {
     return (
         <div className="searchContainer">
             <div className= "searchContent">
-            <img className="img" src={logo} alt="insta" /><h2>|</h2>
-            <h1>Instagram</h1>
+            <img className="img logo" src={logo} alt="insta" />
+            <div className="headline"><p>Instagram</p></div>
                 <form>
                  <input type='search' placeholder='Search'></input>
                 </form>
                 <div className="icons">
-                {/* <FontAwesomeIcon icon="compass" />
-                <FontAwesomeIcon icon="heart" />
-                <FontAwesomeIcon icon="user" /> */}
+                <img className="img heart" src={heart} />
+                <img className="img user" src={user} />
+                <img className="img compass" src={compass} />
                 </div>
         </div>
         </div>
