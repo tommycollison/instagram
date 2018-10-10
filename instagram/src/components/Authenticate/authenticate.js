f3d53ab18348f589a74dd51c0519734a30158055
Tommy Collison <tommy@collison.ie>
2018-10-09 20:12:03 -0700

    starting authenticate

---
+++ instagram/src/components/Authenticate/authenticate.js
@@ -0,0 +1,11 @@
+import React from 'React';
+
+const Authenticate = (App) =>
+    class extends React.Component { 
+        constructor() {
+            return <App />
+        }
+          
+    }
+
+export default Authenticate;
